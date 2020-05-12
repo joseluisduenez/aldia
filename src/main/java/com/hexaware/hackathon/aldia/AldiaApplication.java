cc7b2334565e59f1da1484be00fc3412f70b5ec3
Jose Luis <joseluisduenez@gmail.com>
2020-05-12 12:49:25 -0500

    first commit

---
+++ src/main/java/com/hexaware/hackathon/aldia/AldiaApplication.java
@@ -0,0 +1,13 @@
+package com.hexaware.hackathon.aldia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AldiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AldiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hexaware/hackathon/aldia/AldiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexaware.hackathon.aldia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AldiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
